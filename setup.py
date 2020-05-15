47010b128ae45372022d35ade9d71c1b819497a8
Lukas <lukasgdanietz@gmail.com>
2020-05-15 22:34:44 +0200

    Setup File

---
+++ setup.py
@@ -0,0 +1,22 @@
+from app import db, app, migrate
+from app.models import *
+
+def setup_db():
+
+    user=User(Usuario="GLaDos", email=os.environ.get('SERVER_EMAIL'), name="Caroline")
+    user.set_password("noviembre")
+    admin=Membresia(tipo="Admin")
+    baby=Membresia(tipo="Baby")
+    observer=Membresia(tipo="Observer")
+    full=Membresia(tipo="Full")
+    user.Membresia.append(admin)
+    user.Membresia.append(baby)
+    user.Membresia.append(observer)
+    user.Membresia.append(full)
+    db.session.add(admin)
+    db.session.add(observer)
+    db.session.add(baby)
+    db.session.add(full)
+    db.session.add(user)
+
+setup_db()
